12e254cbe7efa189dcf0dfeac806d376e4c30a88
jenea130 <jenea130@mail.ru>
2024-06-03 16:37:53 +0300

    feat: home-intro pixel perfect

--- template-parts/home/home-intro.php
+++ template-parts/home/home-intro.php
@@ -7,4 +7,8 @@ $image = $home_intro['image'];
 
 <div class="home-intro">
   <img class="home-intro__img" src="<?php echo $image; ?>" alt="">
+  <div class="home-intro__content">
+    <h1 class="home-intro__title"><?php echo $title; ?></h1>
+    <div class="home-intro__text"><?php echo $text; ?></div>
+  </div>
 </div>
