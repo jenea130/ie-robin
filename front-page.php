dc983f12fb56c5284475090d33582169606e9422
jenea130 <jenea130@mail.ru>
2024-06-03 20:12:52 +0300

    feat: philosophy pixel perfect

--- front-page.php
+++ front-page.php
@@ -8,5 +8,6 @@ get_header();
 
 <?php get_template_part('template-parts/home/home-intro'); ?>
 <?php get_template_part('template-parts/home/experience'); ?>
+<?php get_template_part('template-parts/home/philosophy'); ?>
 
 <?php get_footer(); ?>
---
+++ template-parts/home/philosophy.php
@@ -0,0 +1,18 @@
+<?php
+$philosophy = get_field('philosophy');
+$title = $philosophy['title'];
+$image = $philosophy['image'];
+$text = $philosophy['text'];
+$link = $philosophy['link'];
+?>
+
+<div class="philosophy">
+  <img src="<?php echo $image ?>" alt="" class="philosophy__img">
+  <div class="container">
+    <div class="philosophy__content">
+      <h2 class="philosophy__title"><?php echo $title ?></h2>
+      <div class="philosophy__text"><?php echo $text ?></div>
+      <a href="#" class="philosophy__link"><?php echo $link ?></a>
+    </div>
+  </div>
+</div>
