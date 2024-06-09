7183c3e8b641cd36cf95177920f4586635914cf1
jenea130 <jenea130@mail.ru>
2024-06-09 19:45:46 +0300

    upd: main-footer adaptive

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 <?php
 $footer = get_field('footer', 'options');
 $logo = $footer['logo'];
+$socials = $footer['socials'];
 ?>
 
 <footer class="main-footer">
@@ -24,46 +25,60 @@ $logo = $footer['logo'];
         <div class="main-footer__logo">
           <?php echo $logo ?>
         </div>
+        <ul class="main-footer__socials">
+          <?php foreach ($socials as $item) : ?>
+            <?php
+            $url = $item['url'];
+            $type = $item['type'];
+            $icon_url = 'template-parts/icons/icon-' . $type;
+            ?>
+          <li>
+            <a href="<?php echo $url ?>" target="_about">
+            <?php get_template_part($icon_url); ?>
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
       </div>
       <div class="main-footer__col">
-          <?php wp_nav_menu([
-            'theme_location'  => 'footer-1',
-            'menu'            => '',
-            'container'       => '',
-            'container_class' => '',
-            'container_id'    => '',
-            'menu_class'      => 'main-footer__list',
-            'menu_id'         => '',
-            'echo'            => true,
-            'fallback_cb'     => 'wp_page_menu',
-            'before'          => '',
-            'after'           => '',
-            'link_before'     => '',
-            'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            'depth'           => 0,
-            'walker'          => '',
-          ]); ?>
+        <?php wp_nav_menu([
+          'theme_location'  => 'footer-1',
+          'menu'            => '',
+          'container'       => '',
+          'container_class' => '',
+          'container_id'    => '',
+          'menu_class'      => 'main-footer__list',
+          'menu_id'         => '',
+          'echo'            => true,
+          'fallback_cb'     => 'wp_page_menu',
+          'before'          => '',
+          'after'           => '',
+          'link_before'     => '',
+          'link_after'      => '',
+          'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+          'depth'           => 0,
+          'walker'          => '',
+        ]); ?>
       </div>
       <div class="main-footer__col">
-          <?php wp_nav_menu([
-            'theme_location'  => 'footer-2',
-            'menu'            => '',
-            'container'       => '',
-            'container_class' => '',
-            'container_id'    => '',
-            'menu_class'      => 'main-footer__list',
-            'menu_id'         => '',
-            'echo'            => true,
-            'fallback_cb'     => 'wp_page_menu',
-            'before'          => '',
-            'after'           => '',
-            'link_before'     => '',
-            'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            'depth'           => 0,
-            'walker'          => '',
-          ]); ?>
+        <?php wp_nav_menu([
+          'theme_location'  => 'footer-2',
+          'menu'            => '',
+          'container'       => '',
+          'container_class' => '',
+          'container_id'    => '',
+          'menu_class'      => 'main-footer__list',
+          'menu_id'         => '',
+          'echo'            => true,
+          'fallback_cb'     => 'wp_page_menu',
+          'before'          => '',
+          'after'           => '',
+          'link_before'     => '',
+          'link_after'      => '',
+          'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+          'depth'           => 0,
+          'walker'          => '',
+        ]); ?>
       </div>
     </div>
     <span class="main-footer__copyright">Not Copyright 2020 • Robin Williams. Webflow cloneable</span>
